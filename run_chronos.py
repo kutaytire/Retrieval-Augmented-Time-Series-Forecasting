2335934c628eae4648a3f3079f1a83afebba3965
Kutay Tire <kutay.tire@ug.bilkent.edu.tr>
2024-11-24 20:41:45 +0300

    Update run_chronos.py

--- run_chronos.py
+++ run_chronos.py
@@ -1,3 +1,5 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
 import logging
 from pathlib import Path
 from typing import Iterable, Optional
